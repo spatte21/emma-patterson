7c845c09397c60517c21f591c3bf360b7cc75a25
Steve Patterson <spatte21@btinternet.com>
2014-12-15 13:48:04 +0000

    Use hapi.js for view rendering

---
+++ routes/views.js
@@ -0,0 +1,70 @@
+'use strict';
+
+function getSliderImagesStartingAt(firstImage) {
+  var sliderImages = [1, 2, 3, 4, 5, 6, 7, 8];
+  for (var i = 1; i < firstImage; i++) {
+    sliderImages.push(sliderImages.shift());
+  }
+  return sliderImages;
+}
+
+module.exports = function() {
+  return [
+    {
+      method: 'GET',
+      path: '/',
+      handler: function(request, reply) {
+        reply.view('index', {page:'index', sliderImages: getSliderImagesStartingAt(1)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/index',
+      handler: function(request, reply) {
+        reply.view('index', {page:'index', sliderImages: getSliderImagesStartingAt(1)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/eyfs',
+      handler: function(request, reply) {
+        reply.view('eyfs', {page:'eyfs', sliderImages: getSliderImagesStartingAt(2)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/policies',
+      handler: function(request, reply) {
+        reply.view('policies', {page:'policies', sliderImages: getSliderImagesStartingAt(3)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/timesfees',
+      handler: function (request, reply) {
+        reply.view('timesfees', {page: 'timesfees', sliderImages: getSliderImagesStartingAt(4)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/misc',
+      handler: function(request, reply) {
+        reply.view('misc', {page:'misc', sliderImages: getSliderImagesStartingAt(5)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/gallery',
+      handler: function(request, reply) {
+        reply.view('gallery', {page:'gallery', sliderImages: getSliderImagesStartingAt(6)});
+      }
+    },
+    {
+      method: 'GET',
+      path: '/contacts',
+      handler: function(request, reply) {
+        reply.view('contacts', {page:'contacts', sliderImages: getSliderImagesStartingAt(7)});
+      }
+    }
+  ]
+}();
---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var Hapi = require('hapi');
+var Good = require('good');
+var Path = require('path');
+var routes = require('./routes');
+var server = new Hapi.Server();
+
+server.connection({
+  port: process.env.PORT || 3000
+});
+
+var azure = (typeof port === 'string' && port.indexOf('pipe') >= 0);
+
+server.views({
+  engines: {
+    jade: require('jade')
+  },
+  path: Path.join(__dirname, 'views'),
+  compileOptions: {
+    pretty: true
+  }
+});
+
+for (var route in routes) {
+  server.route(routes[route]);
+}
+
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      args: [{
+        log: '*',
+        response: '*'
+      }]
+    }]
+  }
+}, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  if (azure || !module.parent) {
+    server.start(function () {
+      console.log('Server running at: ', server.info.uri);
+    });
+  }
+});
+
+module.exports = server;
