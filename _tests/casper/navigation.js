12cd367ff9ef7a6abd66c5f74a3b4928aca5f36c
Steve Patterson <spatte21@btinternet.com>
2014-12-16 11:36:20 +0000

    Added a singular title css class to each page

--- _tests/casper/navigation.js
+++ _tests/casper/navigation.js
@@ -9,58 +9,79 @@ describe('Basic site navigation', function() {
   });
 
   it('Clicking Home should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Home', 'a', function() {
-        'h2.p2.handwriting'.should.have.text('Dear Parents/Carers,');
-      });
+    casper.then(function() {
+      this.clickLabel('Home', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Home');
+      '.page-title'.should.have.text('Dear Parents/Carers,');
     });
   });
 
   it('Clicking EYFS should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('EYFS', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Early Years Foundation Stage (Revised 2012)');
-      });
+    casper.then(function() {
+      this.clickLabel('EYFS', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('EYFS');
+      '.page-title'.should.have.text('Early Years Foundation Stage (Revised 2012)');
     });
   });
 
   it('Clicking Policies should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Policies', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Policies');
-      });
+    casper.then(function() {
+      this.clickLabel('Policies', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Policies');
+      '.page-title'.should.have.text('Policies');
     });
   });
 
   it('Clicking Times/Fees should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Times/Fees', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Times and Fees');
-      });
+    casper.then(function() {
+      this.clickLabel('Times/Fees', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Times/Fees');
+      '.page-title'.should.have.text('Times and Fees');
     });
   });
 
   it('Clicking Misc. should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Misc.', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Miscellaneous Information');
-      });
+    casper.then(function() {
+      this.clickLabel('Misc.', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Misc.');
+      '.page-title'.should.have.text('Miscellaneous Information');
     });
   });
 
   it('Clicking Gallery should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Gallery', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Our Gallery');
-      });
+    casper.then(function() {
+      this.clickLabel('Gallery', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Gallery');
+      '.page-title'.should.have.text('Our Gallery');
     });
   });
 
   it('Clicking Contacts should display the correct page', function() {
-    casper.then(function(){
-      this.clickLabel('Contacts', 'a', function() {
-        'h2.clr-6.p5.title'.should.have.text('Contact Details');
-      });
+    casper.then(function() {
+      this.clickLabel('Contacts', 'a');
+    });
+
+    casper.then(function() {
+      this.getTitle().should.contain('Contacts');
+      '.page-title'.should.have.text('Contact Details');
     });
   });
 });
--- routes/views.js
+++ routes/views.js
@@ -14,56 +14,56 @@ module.exports = function() {
       method: 'GET',
       path: '/',
       handler: function(request, reply) {
-        reply.view('index', {page:'index', sliderImages: getSliderImagesStartingAt(1)});
+        reply.view('index', {page:'Home', sliderImages: getSliderImagesStartingAt(1)});
       }
     },
     {
       method: 'GET',
       path: '/index',
       handler: function(request, reply) {
-        reply.view('index', {page:'index', sliderImages: getSliderImagesStartingAt(1)});
+        reply.view('index', {page:'Home', sliderImages: getSliderImagesStartingAt(1)});
       }
     },
     {
       method: 'GET',
       path: '/eyfs',
       handler: function(request, reply) {
-        reply.view('eyfs', {page:'eyfs', sliderImages: getSliderImagesStartingAt(2)});
+        reply.view('eyfs', {page:'EYFS', sliderImages: getSliderImagesStartingAt(2)});
       }
     },
     {
       method: 'GET',
       path: '/policies',
       handler: function(request, reply) {
-        reply.view('policies', {page:'policies', sliderImages: getSliderImagesStartingAt(3)});
+        reply.view('policies', {page:'Policies', sliderImages: getSliderImagesStartingAt(3)});
       }
     },
     {
       method: 'GET',
       path: '/timesfees',
       handler: function (request, reply) {
-        reply.view('timesfees', {page: 'timesfees', sliderImages: getSliderImagesStartingAt(4)});
+        reply.view('timesfees', {page: 'Times/Fees', sliderImages: getSliderImagesStartingAt(4)});
       }
     },
     {
       method: 'GET',
       path: '/misc',
       handler: function(request, reply) {
-        reply.view('misc', {page:'misc', sliderImages: getSliderImagesStartingAt(5)});
+        reply.view('misc', {page:'Misc.', sliderImages: getSliderImagesStartingAt(5)});
       }
     },
     {
       method: 'GET',
       path: '/gallery',
       handler: function(request, reply) {
-        reply.view('gallery', {page:'gallery', sliderImages: getSliderImagesStartingAt(6)});
+        reply.view('gallery', {page:'Gallery', sliderImages: getSliderImagesStartingAt(6)});
       }
     },
     {
       method: 'GET',
       path: '/contacts',
       handler: function(request, reply) {
-        reply.view('contacts', {page:'contacts', sliderImages: getSliderImagesStartingAt(7)});
+        reply.view('contacts', {page:'Contacts', sliderImages: getSliderImagesStartingAt(7)});
       }
     }
   ];
